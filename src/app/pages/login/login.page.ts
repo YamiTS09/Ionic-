e9389799c948af3d7094c3235d3b8d55d6aee02b
YamiTS09 <20233L001040@utcv.edu.mx>
2025-01-23 09:13:22 -0600

    Implementar navegacion con navController

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,9 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor() { }
+  constructor(
+    private navController: NavController
+  ) { }
 
   ngOnInit() {
   }
@@ -24,11 +27,19 @@ export class LoginPage implements OnInit {
     //validar formulario
     console.log('valid:', juanito.valid)
 
+    //si form es invalido
+    //console log ('todos los cambios son requeridos')
+    //return
+    if(juanito.invalid){
+      console.log('Todos los cambios son requeridos');
+      return;
+    }
+
     //Si usuario=admin y password=admin
     //Entonces console.log('Login correcto')
     //Sino console.log('Login incorrecto')
     if(this.email==='admin' && this.password==='admin'){
-      console.log('Login correcto')
+      this.navController.navigateForward('/inicio');
     } 
     else{
       console.log('Login incorrecto')
